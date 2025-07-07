1dbeaaa726de354fa124c4d501a405f4ba080192
Mind Mosaic <mindmosaicorg@gmail.com>
2025-07-07 08:20:20 -0700

    rewrite to include other projects

--- include.js
+++ include.js
@@ -1,13 +1,25 @@
 // include.js
 
-fetch('/header.html')
-  .then(res => res.text())
-  .then(text => {
-    document.getElementById('header').innerHTML = text;
-  });
+window.addEventListener("DOMContentLoaded", () => {
+  fetch("header.html")
+    .then(res => {
+      if (!res.ok) throw new Error("Header not found");
+      return res.text();
+    })
+    .then(text => {
+      document.getElementById("header").innerHTML = text;
+      console.log("Header loaded");
+    })
+    .catch(err => console.error(err));
 
-fetch('/footer.html')
-  .then(res => res.text())
-  .then(text => {
-    document.getElementById('footer').innerHTML = text;
-  });
+  fetch("footer.html")
+    .then(res => {
+      if (!res.ok) throw new Error("Footer not found");
+      return res.text();
+    })
+    .then(text => {
+      document.getElementById("footer").innerHTML = text;
+      console.log("Footer loaded");
+    })
+    .catch(err => console.error(err));
+});
